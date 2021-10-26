f6950c570936ec9c0bdb9c447131d477e428acdc
Hamdiyatou adjama <hamdiyatouad@gmail.com>
2021-10-26 08:31:40 +0000

    server

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/gest-stock-project'));
+app.use(express.static('./dist/gest-stock-project'));
 
-app.get('/*', function(req,res) {
+app.get('/*', function (req, res) {
 
-res.sendFile(path.join(__dirname+'/dist/gest-stock-project/index.html'));
+  res.sendFile('index.html', {
+    root: 'dist/gest-stock-project/'
+  })
+  // res.sendFile(path.join(__dirname+'/dist/gest-stock-project/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
